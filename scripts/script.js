867875b0b154e3b10b153adaa59ce47aeb510f8b
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-06-21 13:40:41 +0530

    correction

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const formEl = document.getElementById(`form`);
 const usernameEl = document.getElementById(`username`);
 const emailEl = document.getElementById(`email`);
 const passwordEl = document.getElementById(`password`);
+// const submitEl = document.getElementById(`submit`);
 
 
 
@@ -12,4 +13,7 @@ const passwordEl = document.getElementById(`password`);
 // event listeners
 formEl.addEventListener(`submit`, (event) => {
 event.preventDefault();  
+// const username = usernameEl.value.trim();
+// const email = emailEl.value.trim();
+// const password = passwordEl.value.trim(); 
 })
