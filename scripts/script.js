6122bf6df343127a72c60f865c22547bdadf6633
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-06-21 11:47:27 +0530

    starts javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 `use strict`;
+// select elements
+const formEl = document.getElementById(`form`);
+const usernameEl = document.getElementById(`username`);
+const emailEl = document.getElementById(`email`);
+const passwordEl = document.getElementById(`password`);
+
+
+
+// functions
+
+// event listeners
+formEl.addEventListener(`submit`, (event) => {
+event.preventDefault();  
+})
